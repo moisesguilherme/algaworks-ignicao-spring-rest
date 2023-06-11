b3c48595be6c71ad3dc06a6f26306902d356db8e
Moisés Guilherme <moisescomp@gmail.com>
2023-06-11 11:50:39 -0300

    Configurando e usando o Lombok

--- algatransito-api/src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
+++ algatransito-api/src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
@@ -1,5 +1,10 @@
 package com.algaworks.algatransito.domain.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Proprietario {
 
     private Long id;
@@ -7,36 +12,6 @@ public class Proprietario {
     private String email;
     private String telefone;
 
-    // Getters e Setters
-    public Long getId() {
-        return id;
-    }
+    // retirar Getters e Setters e usar o lombok
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
 }
