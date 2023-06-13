1adbfbf1650029fcdba67979a987cab5b83fa656
Moisés Guilherme <moisescomp@gmail.com>
2023-06-13 06:36:32 -0300

    Implementando endpoint de busca de recurso - part2

--- algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -4,6 +4,7 @@ import com.algaworks.algatransito.domain.model.Proprietario;
 import com.algaworks.algatransito.domain.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,9 +24,14 @@ public class ProprietarioController {
     }
 
     @GetMapping("/proprietarios/{proprietarioId}")
-    public Proprietario buscar(@PathVariable Long proprietarioId){
+    public ResponseEntity<Proprietario> buscar(@PathVariable Long proprietarioId){
         Optional<Proprietario> proprietario = proprietarioRepository.findById(proprietarioId);
-        return proprietario.orElse(null);
+
+        if(proprietario.isPresent()) {
+            return ResponseEntity.ok(proprietario.get());
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
 }
