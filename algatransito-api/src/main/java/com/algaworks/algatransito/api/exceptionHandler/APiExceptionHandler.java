666357544b2ae989606ab5b0d3cd5f897dc821bc
Moisés Guilherme <moisescomp@gmail.com>
2024-06-30 18:53:36 -0300

    5.3. Customizando as informações do ProblemDetail

--- algatransito-api/src/main/java/com/algaworks/algatransito/api/exceptionHandler/APiExceptionHandler.java
+++ algatransito-api/src/main/java/com/algaworks/algatransito/api/exceptionHandler/APiExceptionHandler.java
@@ -1,13 +1,31 @@
 package com.algaworks.algatransito.api.exceptionHandler;
 
 import com.algaworks.algatransito.domain.exception.NegocioException;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.net.URI;
 
 @RestControllerAdvice
 public class APiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setTitle("Um ou mais campos estão inválidos");
+        problemDetail.setType(URI.create("https://algatransito.com/erros/compos-invalidos"));
+
+        return handleExceptionInternal(ex, problemDetail, headers, status, request);
+    }
 
     @ExceptionHandler(NegocioException.class)
     public ResponseEntity<String> capturar(NegocioException e) {
