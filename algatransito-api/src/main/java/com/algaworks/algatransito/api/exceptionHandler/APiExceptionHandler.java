e7c70a491efbbfc44255f7f32eef4e9ad488dc98
Moisés Guilherme <moisescomp@gmail.com>
2024-06-30 19:17:20 -0300

    5.4. Adicionando campos customizados no ProblemDetail

--- algatransito-api/src/main/java/com/algaworks/algatransito/api/exceptionHandler/APiExceptionHandler.java
+++ algatransito-api/src/main/java/com/algaworks/algatransito/api/exceptionHandler/APiExceptionHandler.java
@@ -2,10 +2,12 @@ package com.algaworks.algatransito.api.exceptionHandler;
 
 import com.algaworks.algatransito.domain.exception.NegocioException;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,6 +15,8 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.net.URI;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class APiExceptionHandler extends ResponseEntityExceptionHandler {
@@ -23,6 +27,15 @@ public class APiExceptionHandler extends ResponseEntityExceptionHandler {
         ProblemDetail problemDetail = ProblemDetail.forStatus(status);
         problemDetail.setTitle("Um ou mais campos estão inválidos");
         problemDetail.setType(URI.create("https://algatransito.com/erros/compos-invalidos"));
+
+        Map<String, String> fields = ex.getBindingResult().getAllErrors()
+               .stream()
+               .collect(Collectors.toMap(objectError -> ((FieldError)objectError).getField(),
+                       DefaultMessageSourceResolvable::getDefaultMessage)); //objectError -> objectError.getDefaultMessage()));
+
+               //.forEach( objectError -> System.out.println(((FieldError) objectError).getField() + " - " + objectError.getDefaultMessage()));
+
+        problemDetail.setProperty("fields", fields);
 
         return handleExceptionInternal(ex, problemDetail, headers, status, request);
     }
